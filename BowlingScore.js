a5ea517c4d5be01b26bad727f9c576fd7175d3f2
Unknown <enazarev@gmail.com>
2018-02-13 07:36:08 +0200

    Write regular expressions

--- BowlingScore.js
+++ BowlingScore.js
@@ -1,6 +1,17 @@
 'use strict';
 
-var BowlingScore = () => {
+let roll = '(\\d|-)',
+	strike ='X',
+	spare = `(${roll}/)`,
+	turn = `${strike}|${spare}|${roll}{2}`,
+	rollRX = new RegExp(roll),
+	strikeRX = new RegExp(strike),
+	spareRX = new RegExp(spare),
+	turnRX = new RegExp(turn, 'g');
+
+var BowlingScore = (sequenceFrames) => {
+	let series = sequenceFrames.match(turnRX);
+	return;
 };
 
 try {
