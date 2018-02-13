d7f9bf40c7ff1f924475a1dbfba00e0d594fd266
Unknown <enazarev@gmail.com>
2018-02-13 09:15:09 +0200

    Write script
    Parse string and make calculations

--- BowlingScore.js
+++ BowlingScore.js
@@ -1,17 +1,44 @@
 'use strict';
 
-let roll = '(\\d|-)',
+let roll = '\\d',
 	strike ='X',
 	spare = `(${roll}/)`,
-	turn = `${strike}|${spare}|${roll}{2}`,
+	turn = `${strike}|${spare}|${roll}{2}|${roll}$`,
 	rollRX = new RegExp(roll),
 	strikeRX = new RegExp(strike),
 	spareRX = new RegExp(spare),
-	turnRX = new RegExp(turn, 'g');
+	turnRX = new RegExp(turn, 'gi');
+
+function getTurnData (turnCode) {
+	let isStrike = strikeRX.test(turnCode),
+		isSpare = !isStrike && spareRX.test(turnCode);
+	return {
+		firstRollScore: isStrike? 10 : +turnCode[0],
+		secondRollScore: isStrike? 0 : isSpare? 10-turnCode[0] : +turnCode[1],
+		isSpare,
+		isStrike
+	};
+}
 
 var BowlingScore = (sequenceFrames) => {
-	let series = sequenceFrames.match(turnRX);
-	return;
+	let series = sequenceFrames
+		.replace(/-/g, '0')
+		.match(turnRX);
+
+	return series
+			.map((curentTurnCode) => getTurnData(curentTurnCode))
+			.reduce((totalScore, current, i, array) => {
+				if(i>9){return totalScore};
+
+				function lookAhead() {
+					return  current.isSpare  ? array[i+1].firstRollScore :
+							current.isStrike ?
+								(array[i+1].isStrike? 10 + array[i+2].firstRollScore : array[i+1].firstRollScore + array[i+1].secondRollScore)
+							:0;
+				}
+
+				return totalScore + (current.firstRollScore + current.secondRollScore) + lookAhead() ;
+			}, 0);
 };
 
 try {
