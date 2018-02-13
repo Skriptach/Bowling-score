a32522e735e3cb70486ff68fa016a47adac5f0df
Unknown <enazarev@gmail.com>
2018-02-13 07:35:17 +0200

    Change last turn format string

--- spec/BowlingScore_spec.js
+++ spec/BowlingScore_spec.js
@@ -16,14 +16,14 @@ describe('Bowling-score', () => {
 		expect(BowlingScore('18 27 36 45 54 63 72 81 -9 9-')).toBe(90);
 	});
 	it('all pairs of 5 and spare, with a final 5 should be equal 150', () => {
-		expect(BowlingScore('5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5')).toBe(150);
+		expect(BowlingScore('5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5')).toBe(150);
 	});
 	it('intervaling spare and non-spare should be equal 96', () => {
 		expect(BowlingScore('5/ 4- 4/ -5 5/ 36 6/ 23 5/ 7-')).toBe(96);
 		// 10+4, 4, 10+0, 5, 10+3, 9, 10+2, 5, 10+7, 7 
 	});
 	it('intervaling strikes and non-strikes should be equal 156', () => {
-		expect(BowlingScore('X 34 X -5 X 72 X X 6/ X44')).toBe(156);
+		expect(BowlingScore('X 34 X -5 X 72 X X 6/ X 44')).toBe(156);
 		// 10+3+4, 7, 10+0+5, 5, 10+7+2, 9, 10+10+6, 10+6+4, 10+10, 10+4+4
 	});
 });
